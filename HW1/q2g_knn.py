2eec00d42e05c3b949cd67872e91b963ee113d4d
gefen keinan <gefenk9@gmail.com>
2019-03-26 12:56:13 +0200

    knn

--- HW1/q2g_knn.py
+++ HW1/q2g_knn.py
@@ -1,6 +1,14 @@
 import numpy as np
 from q2e_word2vec import normalizeRows
 
+def cos_sim(a, b):
+	"""Takes 2 vectors a, b and returns the cosine similarity according
+	to the definition of the dot product
+	"""
+	dot_product = np.dot(a, b)
+	norm_a = np.linalg.norm(a)
+	norm_b = np.linalg.norm(b)
+	return dot_product / (norm_a * norm_b)
 
 def knn(vector, matrix, k=10):
     """
@@ -18,8 +26,15 @@ def knn(vector, matrix, k=10):
     nearest_idx = []
 
     ### YOUR CODE
+    score = []
+    for index, row in enumerate(matrix):
+        score.append((cos_sim(row, vector), index))
     ### END YOUR CODE
+    sorted_vectors = sorted(score, key=lambda x:x[0], reverse=True)
+    for i in range(k):
+        nearest_idx.append(sorted_vectors[i][1])
     return nearest_idx
+
 
 def test_knn():
     """
