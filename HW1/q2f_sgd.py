1c8d25671333e5ade6519cf6f6f706219cf479c9
gefen keinan <gefenk9@gmail.com>
2019-03-26 11:48:47 +0200

    2f

--- HW1/q2f_sgd.py
+++ HW1/q2f_sgd.py
@@ -85,7 +85,9 @@ def sgd(f, x0, step, iterations, postprocessing=None, useSaved=False,
 
         cost = None
         ### YOUR CODE HERE
-        raise NotImplementedError
+        cost, gradient = f(x)
+        x -= gradient * step
+        postprocessing(x)
         ### END YOUR CODE
 
         if iter % PRINT_EVERY == 0:
@@ -132,7 +134,7 @@ def your_sanity_checks():
     """
     print "Running your sanity checks..."
     ### YOUR CODE HERE
-    raise NotImplementedError
+
     ### END YOUR CODE
 
 
